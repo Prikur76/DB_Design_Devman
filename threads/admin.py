e8cde68bf5ef2df5b47d85c4d88a94d246fbbd2c
Vladimir Shisterov <v.shisterov76u@gmail.com>
2025-05-06 17:39:44 +0300

    add models Department, User, Course, EducationThread

--- threads/admin.py
+++ threads/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import (
+    User, Department
+)
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    ... 
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    ... 
